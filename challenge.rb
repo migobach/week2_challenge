e629d2b1129f7469b1f9397e141ba4ebdb607578
Michael Bach <migobach@gmail.com>
2018-05-18 12:36:51 -0600

    use each_char

--- challenge.rb
+++ challenge.rb
@@ -7,33 +7,21 @@
 require 'pry'
 
 @letters = []
+@test = []
 
 def test
- puts "Input a string to see if it is a palindrome"
- str = gets.strip.downcase
-  str.each_char { |c|  @letters << c }
- puts "#{@letters}"
-#  reverse_string
- binding.pry
+  puts "Input a string to see if it is a palindrome"
+  @str = gets.strip.downcase.gsub(" ", "") 
+  @str.each_char { |c|  @letters << c }
+  reverse
 end
 
-
-# use .split("")
-
-# def reverse(letters)
-#   loop = letters.length
-# end
-
-
-# def reverse_string(letters) # method reverse_string with parameter 'string'
-#   loop = letters.length
-#   binding.pry       # int loop is equal to the string's length
-#   word = ''                  # this is what we will use to output the reversed word
-#   while loop > 0             # while loop is greater than 0, subtract loop by 1 and add the string's index of loop to 'word'
-#     loop -= 1                  # subtract 1 from loop
-#     word += string[loop]       # add the index with the int loop to word
-#   end                        # end while loop
-#   return word                # return the reversed word
-# end
-
-test 
+def reverse
+  @letters.each {|l| @test.unshift(l) }
+  if @letters == @test
+    puts "It is a palindrome!"
+  else 
+    puts "Sorry, try again."
+  end
+end
+test
